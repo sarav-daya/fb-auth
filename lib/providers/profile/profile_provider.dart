bfd3876847925bc2dba2e9ac5c3af3fe7490cea8
Saravanan <saravanan.dayalan@outlook.com>
2022-02-04 07:03:43 +0530

    profile state and provider added

---
+++ lib/providers/profile/profile_provider.dart
@@ -0,0 +1,28 @@
+import 'package:fb_auth_provider/models/custom_error.dart';
+import 'package:flutter/cupertino.dart';
+
+import 'package:fb_auth_provider/providers/profile/profile_state.dart';
+import 'package:fb_auth_provider/repositories/profile_repository.dart';
+
+class ProfileProvider with ChangeNotifier {
+  ProfileState _state = ProfileState.initial();
+  ProfileState get state => _state;
+
+  final ProfileRepository profileRepository;
+  ProfileProvider({
+    required this.profileRepository,
+  });
+
+  Future<void> getProfile({required String uid}) async {
+    _state = _state.copyWith(profileStatus: ProfileStatus.loading);
+    notifyListeners();
+
+    try {
+      final user = await profileRepository.getProfile(uid: uid);
+      _state = _state.copyWith(profileStatus: ProfileStatus.loaded, user: user);
+    } on CustomError catch (e) {
+      _state = _state.copyWith(profileStatus: ProfileStatus.error, error: e);
+      notifyListeners();
+    }
+  }
+}
